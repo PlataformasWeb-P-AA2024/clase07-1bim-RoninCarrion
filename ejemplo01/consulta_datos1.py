1f64ddf68e6d0dac9c2e5add7d7370a2b79c89b5
Ronin Carrión <rdcarrion3@utpl.edu.ec>
2024-05-20 20:15:40 +0000

    Manejo de consultas en sqlalchemy

--- ejemplo01/consulta_datos1.py
+++ ejemplo01/consulta_datos1.py
@@ -23,12 +23,14 @@ clubs = session.query(Club).all()
 print(clubs)
 # Se recorre la lista a través de un ciclo
 # repetitivo for en python
-# print("Presentación de Clubs")
-# for s in clubs:
-#     print("%s" % (s.nombre))
-#     print("%s" % (s.id))
-#     print("---------")
+print("Presentación de Clubs")
+for s in clubs:
+    print("%s" % (s.nombre))
+    print("%s" % (s.id))
+    print("%s" % (s.fundacion))
+    print("---------")
 
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 # Obtener todos los registros de 
 # la entidad Jugador
 jugadores = session.query(Jugador).all()
